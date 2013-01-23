c356e1720f6270a8327f3a09f943d33046e4587c
Riley Teige <riley.teige@gmail.com>
2013-01-22 21:09:48 -0800

    Providing AppCommand, ViewModelBase.

---
+++ MVVMBase/Command/AppCommand.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace MVVMBase.Command
+{
+	/// <summary>
+	/// A command whose sole purpose is to
+	/// relay its functionality to other
+	/// objects by invoking delegates. The
+	/// default return value for CanExecute()
+	/// is true.
+	/// </summary>
+	public class AppCommand : ICommand
+	{
+		#region Fields
+
+		Action<object> mExecute;
+		Predicate<object> mCanExecute;
+
+		#endregion // Fields
+
+		#region Constructors
+
+		public AppCommand(Action<object> execute)
+			: this(execute, null)
+		{
+		}
+
+		public AppCommand(Action<object> execute, Predicate<object> canExecute)
+		{
+			if (execute == null)
+				throw new ArgumentNullException("execute");
+
+			mExecute = execute;
+			mCanExecute = canExecute;
+		}
+
+		#endregion // Constructors
+
+		#region ICommand Members
+
+		[DebuggerStepThrough]
+		public bool CanExecute(object parameter)
+		{
+			return mCanExecute == null ? true : mCanExecute(parameter);
+		}
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public void Execute(object parameter)
+		{
+			mExecute(parameter);
+		}
+
+		#endregion
+	}
+}
---
+++ MVVMBase/ViewModel/ViewModelBase.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace MVVMBase.ViewModel
+{
+	/// <summary>
+	/// Base class for all ViewModel classes in the app.
+	/// It provides support for property change notifications
+	/// and has a DisplayName property. This class is abstract.
+	/// </summary>
+	public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+	{
+		#region Constructor
+
+		protected ViewModelBase()
+		{
+		}
+
+		#endregion // Constructor
+
+		#region Debugging Aides
+
+		/// <summary>
+		/// Returns whether an exception is thrown, or if a Debug.Fail() is used
+		/// when an invalid property name is passed to the VerifyPropertyName method.
+		/// The default value is false, but subclasses used by unit tests might
+		/// override this property's getter to return true.
+		/// </summary>
+		protected virtual bool ThrowOnInvalidPropertyName { get; private set; }
+
+		/// <summary>
+		/// Warns the developer if this object does not have
+		/// a public property with the specified name. This
+		/// method does not exist in a Release build.
+		/// </summary>
+		/// <param name="propertyName">The property name to check.</param>
+		[Conditional("DEBUG")]
+		[DebuggerStepThrough]
+		public void VerifyPropertyName(string propertyName)
+		{
+			// Verify that the property name matches a real,
+			// public, instance property on this object.
+			if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+			{
+				string msg = "Invalid property name: " + propertyName;
+				if (this.ThrowOnInvalidPropertyName)
+					throw new Exception(msg);
+				else
+					Debug.Fail(msg);
+			}
+		}
+
+		#endregion // Debugging Aides
+
+		#region INotifyPropertyChanged Members
+
+		/// <summary>
+		/// Raised when a property on this object has a new value.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// Raises this object's PropertyChanged event.
+		/// </summary>
+		/// <param name="propertyName">The property that has a new value.</param>
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			this.VerifyPropertyName(propertyName);
+
+			PropertyChangedEventHandler handler = this.PropertyChanged;
+			if (handler != null)
+			{
+				PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
+				handler(this, e);
+			}
+		}
+
+		#endregion // INotifyPropertyChangedMembers
+
+		#region IDisposable Members
+
+		/// <summary>
+		/// Invoked when this object is being removed from the app
+		/// and will be subject to garbage collection.
+		/// </summary>
+		public void Dispose()
+		{
+			this.OnDispose();
+		}
+
+		/// <summary>
+		/// Child classes can override this method to perform
+		/// clean-up logic, such as removing event handlers.
+		/// </summary>
+		protected virtual void OnDispose()
+		{
+		}
+
+		#endregion // IDisposable Members
+	}
+}
